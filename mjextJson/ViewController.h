c7e61b0e45e5c4f012e128942cbd136818b068fb
democ <akeysong@gmail.com>
2016-08-22 16:31:54 +0800

    Initial Commit

---
+++ mjextJson/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mjextJson
+//
+//  Created by apple on 16/8/22.
+//  Copyright © 2016年 zhanyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mjextJson/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mjextJson
+//
+//  Created by apple on 16/8/22.
+//  Copyright © 2016年 zhanyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
